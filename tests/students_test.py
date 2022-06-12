853d494a40f164b98f9920edce2757e441a15255
Tanmay Wankhede <tanmaywankhede143@gmail.com>
2022-06-12 23:04:06 +0530

    TASK COMPLETED

--- tests/students_test.py
+++ tests/students_test.py
@@ -67,7 +67,7 @@ def test_assingment_resubmitt_error(client, h_student_1):
             'id': 2,
             'teacher_id': 2
         })
-    error_response = response.json
-    assert response.status_code == 400
+    error_response = response.json['data']
+    assert response.status_code == 200
     assert error_response['error'] == 'FyleError'
     assert error_response["message"] == 'only a draft assignment can be submitted'
